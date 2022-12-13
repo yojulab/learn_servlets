2bdca9dd4b93347acb88ed502de71850a1b999f6
Sanghun Oh <otter.oh@gmail.com>
2022-12-13 20:45:05 +0900

    add servlet filters

---
+++ src/main/java/jrout/tutorial/springbootservletjsp/SimpleFilter.java
@@ -0,0 +1,33 @@
+// https://www.tutorialspoint.com/spring_boot/spring_boot_servlet_filter.htm
+package jrout.tutorial.springbootservletjsp;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleFilter implements Filter {
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterchain)
+            throws IOException, ServletException {
+
+        System.out.println("Remote Host:" + request.getRemoteHost());
+        System.out.println("Remote Address:" + request.getRemoteAddr());
+        filterchain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterconfig) throws ServletException {
+    }
+}
